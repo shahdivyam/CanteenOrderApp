03096a72ff3e619b2fff15f8a709fa9ef5ead4c8
Anthony Zheng <Anthony@fopen-dream.space>
2016-10-28 19:54:28 -0400

    Responsive Design, Code Refactor, Main page redesin

--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,27 @@
 // JavaScript Document
 
-$("document").ready(function(){
-    "use strict";
-    
-    $(".popular").click(function(){
-        console.log("popular was clicked");
-        $("#popularBlock").css("left","0px");
-$(".popular").addClass("activeButton");
-        $("#popularBlock").css("display","inherit");
-        $("#menuBlock").css("left","3000px");
-        $("#menuBlock").css("display","none");
-        $("#cartBlock").css("left","3000px");
-        $("#cartBlock").css("display","none");
-        $(".menu").removeClass("activeButton");
-        $(".cart").removeClass("activeButton");
-        
+$(function() {
+    $("#links").affix({
+        offset: {
+            top: $(window).height()
+        }
     });
-    
-    $(".menu").click(function(){
-        console.log("menu was clicked");
-        $("#menuBlock").css("left","0px");
-$(".menu").addClass("activeButton");
-        $("#menuBlock").css("display","inherit");
-        $("#popularBlock").css("left","-3000px");
-        $("#popularBlock").css("display","none");
-        $("#cartBlock").css("left","3000px");
-        $("#cartBlock").css("display","none");
-        $(".menu").removeClass("activeButton");
-        $(".cart").removeClass("activeButton");
-        
+
+    $(".popular").on("click", function(){
+        $("#popularBlock").removeClass("hidden");
+        $("#menuBlock").addClass("hidden");
+        $("#cartBlock").addClass("hidden");
     });
-    $(".cart").click(function(){
-        console.log("cart clicked");
-        $("#cartBlock").css("display","inherit");
-        $("#cartBlock").css("left","0px");
-        $("#popularBlock").css("left","-3000px");
-        $("#popularBlock").css("display","none");
-        $("#menuBlock").css("display","none");
-        $("#menuBlock").css("left","0px");
-        
+
+    $(".menu").on("click", function(){
+        $("#menuBlock").removeClass("hidden");
+        $("#popularBlock").addClass("hidden");
+        $("#cartBlock").addClass("hidden");
+    }); 
+
+    $(".cart").on("click", function(){
+        $("#cartBlock").removeClass("hidden");
+        $("#popularBlock").addClass("hidden");
+        $("#menuBlock").addClass("hidden");
     });
-    
-    
 });
